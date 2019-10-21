5f4f1e48b55b96b19104ac8426f1527f0e6487ec
Jose Suresh <suale96@gmail.com>
2019-10-21 20:47:37 +0100

    V1.0

---
+++ juegoSurvivalZombie.sql
@@ -0,0 +1,28 @@
+drop database if exists juego;
+create database juego;
+use juego;
+-- -----------------------------------------------------
+-- Schema juego
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `juego` DEFAULT CHARACTER SET utf8 ;
+USE `juego` ;
+
+-- -----------------------------------------------------
+-- Table `juego`.`imagenes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `juego`.`imagenes` (
+  `id_Img` INT NOT NULL,
+  `ruta_img` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_Img`))
+ENGINE = InnoDB;
+
+INSERT INTO juego.imagenes VALUES(1,"./src/images/soldier.png");
+INSERT INTO juego.imagenes VALUES(2,"./src/images/skeleton-attack.png");
+INSERT INTO juego.imagenes VALUES(3,"./src/images/background.png");
+INSERT INTO juego.imagenes VALUES(4,"./src/images/Hearth.png");
+INSERT INTO juego.imagenes VALUES(5, "./src/images/bullet.png");
+INSERT INTO juego.imagenes VALUES(6, "./src/images/pink_soldier.png");
+INSERT INTO juego.imagenes VALUES(7, "./src/images/bg.png");
+
+
+select * from juego.imagenes;
